b0e2a61e2d4e995f9593ae94025107a2a6fef571
paulwallin <steely_paul@hotmail.com>
2019-10-04 13:45:59 +0300

    init and transitions

---
+++ components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+      <div class="nav">
+        <nuxt-link to="/" class="brand">HelloNuxt</nuxt-link>
+        <nav>
+          <nuxt-link to="/">Hello</nuxt-link>&nbsp;|
+          <nuxt-link to="/create">About</nuxt-link>
+        </nav>
+      </div>
+    </template>
---
+++ pages/create.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>About</h1>
+    <p>
+      Testin NUXT.js with animated transitions
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  transition: 'bounce'
+}
+</script>
