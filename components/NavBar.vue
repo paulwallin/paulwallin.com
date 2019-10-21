9a16bc387dbdb4f86b9966fdb77657728074384c
paulwallin <steely_paul@hotmail.com>
2019-10-21 14:11:13 +0300

    updated navbar komponent

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,8 +2,8 @@
       <div class="nav">
         <nuxt-link to="/" class="brand">paulwallin.com</nuxt-link>
         <nav>
-          <nuxt-link to="/">Hello</nuxt-link>&nbsp;|
-          <nuxt-link to="/create">About</nuxt-link>
+          <nuxt-link to="/">Hello!</nuxt-link>&nbsp;|
+          <nuxt-link to="/create">About this site</nuxt-link>
         </nav>
       </div>
     </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container">
-    <h1>Hello ;)</h1>
+    <h1>Hello!</h1>
   </div>
 </template>
