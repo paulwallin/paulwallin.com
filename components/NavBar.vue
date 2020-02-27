a9113f20bf6ebd4c9dc53b15c55992bdcd0a8e72
paulwallin <steely_paul@hotmail.com>
2020-02-27 10:07:14 +0200

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
-      <div class="nav">
-        <nuxt-link to="/" class="brand">paulwallin.com</nuxt-link>
-        <nav>
-          <nuxt-link to="/">Hello!</nuxt-link>&nbsp;|
-          <nuxt-link to="/create">About this site</nuxt-link>
-        </nav>
-      </div>
-    </template>
+ <div class="nav">
+ <nuxt-link to="/" class="brand">paulwallin.com</nuxt-link>
+ <nav>
+ <nuxt-link to="/">Hello!</nuxt-link>&nbsp;|
+ <nuxt-link to="/create">About this site</nuxt-link>
+  </nav>
+  </div>
+</template>
