3a2262b7d75c8c35e99d4e1d931b1b5e0df58956
paulwallin <steely_paul@hotmail.com>
2020-02-27 10:44:29 +0200

    structure and nav

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,15 @@
 <template>
- <div class="nav">
- <nuxt-link to="/" class="brand">paulwallin.com</nuxt-link>
- <nav>
- <nuxt-link to="/">Hello!</nuxt-link>&nbsp;|
- <nuxt-link to="/create">About this site</nuxt-link>
-  </nav>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">
+      paulwallin.com
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/">
+        Hello!
+      </nuxt-link>
+      <nuxt-link to="/about">
+        About
+      </nuxt-link>
+    </nav>
   </div>
 </template>
